55e4847650780469d8475d01e73bc5cd4013e3f5
杨茗凯 <yangmingkai@shuidihuzhu.com>
2020-11-17 17:52:20 +0800

    add ch02

---
+++ ch02/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	cmd := parseCmd()
+	if cmd.versionFlag {
+		fmt.Println("version 0.0.1 by ymk 2020")
+	} else if cmd.helpFlag || cmd.class == "" {
+		printUsage()
+	} else {
+		startJVM(cmd)
+	}
+}
